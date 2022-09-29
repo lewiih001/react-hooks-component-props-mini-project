afcc79fabf7b6608c8ca688e8fe5a9c67ce67458
lewiih001 <lewismunyambu@gmail.com>
2022-09-29 13:03:25 +0300

    completed assignment

---
+++ src/components/Article.js
@@ -0,0 +1,15 @@
+function Article({title,date,preview}){
+    
+    
+    return (
+        <article>
+            <h3>{title}</h3>
+            { date ? <small>{date}</small> : 
+              <small>January 1, 1970</small>
+            }
+            <p>{preview}</p>
+        </article>
+    )
+}
+
+export default Article;
